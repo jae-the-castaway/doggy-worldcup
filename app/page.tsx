e81acfe1cce6001b8eaf8152fa234084bb29f0aa
Jaeheon Kim <jaeheonkim95@gmail.com>
2023-07-24 12:07:16 -0500

    change url

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import Match from "./match";
-import { StrictMode } from 'react';
+import { StrictMode } from "react";
 
 const url =
   "https://api.thedogapi.com/v1/images/search?limit=16&has_breeds=1&order=random";
 
 export default async function Home() {
-  const data = await fetch(
-    `https://api.thedogapi.com/v1/images/search?limit=16&has_breeds=1&order=random&api_key=${process.env.API_KEY}`,
-    { next: { revalidate: 10 } }
-  );
+  const data = await fetch(`${url}&api_key=${process.env.API_KEY}`, {
+    next: { revalidate: 10 },
+  });
   const res = await data.json();
 
   return (
     <StrictMode>
-    <main className="h-screen">
-      <Match dogs={res} />
-    </main>
+      <main className="h-screen">
+        <Match dogs={res} />
+      </main>
     </StrictMode>
   );
 }
-
-// change env.local
